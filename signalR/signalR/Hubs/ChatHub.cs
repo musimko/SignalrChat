82fef8c814e18507484b8bdd78ca0343432e8ae8
sami <smile_666@naver.com>
2019-12-19 01:15:16 +0900

    add

---
+++ signalR/signalR/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+//서버
+namespace signalR.Hubs
+{
+    [HubName("chatt")]
+    //클라이언트에서 사용할때는 chat_samii이라는 이름으로 보겟다.
+    public class ChatHub : Hub
+    {
+        public void Hello(string msg)
+        {
+            Clients.All.hello(msg);
+        }
+    }
+}
---
+++ signalR/signalR/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace signalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
